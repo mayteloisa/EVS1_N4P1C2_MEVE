b18c583f13fe6dd21676f5b45412abf6ad4dd18e
mayté valenzuela <mayteqqeloisa@gmail.com>
2023-09-25 15:38:05 -0300

    app1listitasi

--- app1/views.py
+++ app1/views.py
@@ -5,11 +5,17 @@ from django.http import HttpResponse
 def vistaUno(request):
     html="""
     <h1 style="color:blue"> holaaaaaa mundito </h1>
+    <h2 style="color:blue"> holaaaaaa munditotitoooooo </h2>
+    <h3 style="color:blue"> holaaaaaa mundito holiwissss </h3>
+    <h4 style="color:blue"> holaaaaaa mundito holissssss </h4>
     """
     return HttpResponse(html)
 
 def vistaDos (request):
     vista2="""
     <h4 style="color:red"> viva la programación </h4>
+    <h1 style="color:red"> viva la vida </h1>
+    <h3 style="color:red"> viva la fiesta </h3>
+    <h4 style="color:red"> viva la wtf?</h4>
     """
     return HttpResponse(vista2)
